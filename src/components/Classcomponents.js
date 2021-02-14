cb6530e844050b51ad95aea349e2e929a279659b
MithunW <mithun.17@cse.mrt.ac.lk>
2021-02-15 01:18:32 +0530

    functions and components

---
+++ src/components/Classcomponents.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Classcomponents extends Component {
+    
+    render() {
+        return (
+            <div>
+              <h1>This is a class component</h1>  
+            </div>
+        )
+    }
+}
+
+export default Classcomponents
